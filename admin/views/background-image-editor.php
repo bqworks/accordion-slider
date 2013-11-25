34ed3b4b3dd2e5915373aab57e923b214ffc61c2
davidghi <david.ghiurau@gmail.com>
2013-11-25 12:11:15 +0200

    change background-opened to opened-background

--- admin/views/background-image-editor.php
+++ admin/views/background-image-editor.php
@@ -27,8 +27,8 @@
 		</table>
 	</div>
 
-	<div class="fieldset background-opened-image">
-		<h3 class="heading">Background Opened Image</h3>
+	<div class="fieldset opened-background-image">
+		<h3 class="heading">Opened Background Image</h3>
 		<div class="image-loader"></div>
 		<table class="data-fields">
 			<tbody>
@@ -69,7 +69,7 @@
 	</div>
 
 	<div class="buttons">
-		<a class="button-secondary add-background-opened-image" href="#">Add Background Opened Image</a>
+		<a class="button-secondary add-opened-background-image" href="#">Add Opened Background Image</a>
 		<a class="button-secondary add-link" href="#">Add Link</a>
 		<a class="button-secondary save" href="#">Save</a>
 		<a class="button-secondary close" href="#">Close</a>
