35050137ac299745f77e3b92a432299994ae238b
davidghi <david.ghiurau@gmail.com>
2014-03-04 18:18:51 +0200

    add implementation for uninstall.php

--- uninstall.php
+++ uninstall.php
@@ -1,5 +1,49 @@
 <?php
 
 if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) {
-	exit;
+	exit();
+}
+
+if ( function_exists( 'is_multisite' ) && is_multisite() ) {
+	global $wpdb;			
+	$blog_ids = $wpdb->get_col( "SELECT blog_id FROM $wpdb->blogs" );
+	
+	if ( $blog_ids !== false ) {
+		foreach ( $blog_ids as $blog_id ) {
+			switch_to_blog( $blog_id );
+			bqw_accordion_slider_delete_all_data();
+		}
+
+		restore_current_blog();
+	}
+} else {
+	bqw_accordion_slider_delete_all_data();
+}
+
+function bqw_accordion_slider_delete_all_data() {
+	global $wpdb;
+	$prefix = $wpdb->prefix;
+
+	$accordions_table = $prefix . 'accordionslider_accordions';
+	$panels_table = $prefix . 'accordionslider_panels';
+	$layers_table = $prefix . 'accordionslider_layers';
+
+	$wpdb->query( "DROP TABLE $accordions_table, $panels_table, $layers_table" );
+
+	delete_option( 'accordion_slider_custom_css' );
+	delete_option( 'accordion_slider_custom_js' );
+	delete_option( 'accordion_slider_is_custom_css' );
+	delete_option( 'accordion_slider_is_custom_js' );
+	delete_option( 'accordion_slider_load_stylesheets' );
+	delete_option( 'accordion_slider_load_custom_css_js' );
+	delete_option( 'accordion_slider_load_unminified_scripts' );
+	delete_option( 'accordion_slider_purchase_code' );
+	delete_option( 'accordion_slider_purchase_code_message' );
+	delete_option( 'accordion_slider_purchase_code_status' );
+	
+	delete_transient( 'accordion_slider_post_names' );
+	delete_transient( 'accordion_slider_posts_data' );
+	delete_transient( 'accordion_slider_update_notification_message' );
+	
+	$wpdb->query( "DELETE FROM " . $prefix . "options WHERE option_name LIKE '%accordion_slider_cache%'" );
 }
