98b4aa8edbc47eaf863f9763783400e67f07e77c
davidghi <david.ghiurau@gmail.com>
2014-03-06 11:35:53 +0200

    add possibility to load translation from the plugin's folder

--- public/class-accordion-slider.php
+++ public/class-accordion-slider.php
@@ -125,6 +125,7 @@ class BQW_Accordion_Slider {
 		$locale = apply_filters( 'plugin_locale', get_locale(), $domain );
 
 		load_textdomain( $domain, trailingslashit( WP_LANG_DIR ) . $domain . '/' . $domain . '-' . $locale . '.mo' );
+		load_plugin_textdomain( $domain, false, 'accordion-slider/languages/' );
 	}
 
 	/**
