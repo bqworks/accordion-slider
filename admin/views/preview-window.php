16533244594326295d97ea1f1bf94585bf1dc3cf
davidghi <david.ghiurau@gmail.com>
2014-03-10 18:07:09 +0200

    remove preview window heading

--- admin/views/preview-window.php
+++ admin/views/preview-window.php
@@ -2,7 +2,6 @@
 <div class="modal-window-container preview-window">
 	<div class="modal-window">
 		<span class="close-x"></span>
-		<h3 class="heading"><?php echo $accordion_name; ?></h3>
 		<?php echo $accordion_output; ?>
 	</div>
 </div>
