76e49c9fb1580e6937da65846ae2d8f43b26b2a4
davidghi <david.ghiurau@gmail.com>
2014-03-10 18:09:15 +0200

    remove import and export windows heading

--- admin/views/export-window.php
+++ admin/views/export-window.php
@@ -2,7 +2,7 @@
 <div class="modal-window-container export-window">
 	<div class="modal-window">
 		<span class="close-x"></span>
-		<h3 class="heading"><?php _e( 'Export accordion', 'accordion-slider' ); ?></h3>
+		
 		<textarea><?php echo isset( $export_string ) ? $export_string : ''; ?></textarea>
 	</div>
 </div>
--- admin/views/import-window.php
+++ admin/views/import-window.php
@@ -2,7 +2,7 @@
 <div class="modal-window-container import-window">
 	<div class="modal-window">
 		<span class="close-x"></span>
-		<h3 class="heading"><?php _e( 'Import accordion', 'accordion-slider' ); ?></h3>
+		
 		<textarea></textarea>
 		<div class="buttons">
 			<a class="button-secondary save" href="#"><?php _e( 'Import', 'accordion-slider' ); ?></a>
