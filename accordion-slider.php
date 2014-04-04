6619a3a248d594c764de61492bd8dc21fa5eeb00
davidghi <david.ghiurau@gmail.com>
2014-04-04 20:52:57 +0300

    increment version

--- accordion-slider.php
+++ accordion-slider.php
@@ -4,7 +4,7 @@
 	Plugin Name: Accordion Slider
 	Plugin URI:  http://bqworks.net/accordion-slider/
 	Description: Responsive and touch-enabled accordion slider.
-	Version:     1.0
+	Version:     1.0.1
 	Author:      bqworks
 	Author URI:  http://bqworks.com
 */
