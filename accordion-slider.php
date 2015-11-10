f55c620b61c8f2da400b2af57f56d9b436044a3b
David Ghiurau <david.ghiurau@gmail.com>
2015-11-10 18:09:43 +0200

    correct plugin uri

--- accordion-slider.php
+++ accordion-slider.php
@@ -2,7 +2,7 @@
 
 /*
 	Plugin Name: Accordion Slider
-	Plugin URI:  http://bqworks.net/accordion-slider/
+	Plugin URI:  http://bqworks.com/accordion-slider/
 	Description: Responsive and touch-enabled accordion slider.
 	Version:     1.3
 	Author:      bqworks
