f11de1b22f1c83c487f8487f98e8d447600395b4
David Ghiurau <contact@bqworks.net>
2022-09-19 20:16:41 +0300

    fix typo

--- gutenberg/build/index.asset.php
+++ gutenberg/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'c6b765ab34c091b8ced8');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'f9f26a9e85577a26c887');
