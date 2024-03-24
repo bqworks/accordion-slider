00d18e0393c2dd36fba8870d7a401989b1ddae08
Grégory Parrot <gparrot@xternalsoft.com>
2024-03-24 02:39:50 +0100

    Add description

--- admin/views/panel-settings/posts-ids-panel-settings.php
+++ admin/views/panel-settings/posts-ids-panel-settings.php
@@ -4,5 +4,6 @@
 	</td>
 	<td class="setting-cell">
 		<input id="posts-ids" class="panel-setting" type="text" name="posts_ids" value="<?php echo isset( $panel_settings['posts_ids'] ) ? esc_attr( $panel_settings['posts_ids'] ) : esc_attr( $panel_default_settings['posts_ids']['default_value'] ); ?>" />
+        <p class="description"><?php echo $panel_default_settings['posts_ids']['description']; ?></p>
 	</td>
 </tr>
